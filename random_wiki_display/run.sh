50abe2c1ba1d6adbc6b9798f8009b2859aebfba1
ta0ma0 <exemple@exemple.com>
2026-02-11 13:37:32 +0300

    fix venv path

--- random_wiki_display/run.sh
+++ random_wiki_display/run.sh
@@ -6,7 +6,6 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # 2. Переходим в директорию проекта (чтобы скрипт видел .env и json-файлы)
 cd "$PROJECT_DIR"
-cd ".."
 
 # 3. Определяем путь к Python внутри venv
 VENV_PYTHON="$PROJECT_DIR/venv/bin/python"
